87e5375cebb6725401e9c6779f5aa078e74de194
Sabeel Ahmad <sabeelbasharat@gmail.com>
2018-08-19 21:14:24 +0530

    updated add_book func

--- functions.py
+++ functions.py
@@ -2,6 +2,21 @@
 
 import student
 import faculty
+import book
+
+
+def check_if_isbn_present(isbn):
+    for bk in book.book_list:
+        if bk.isbn == isbn:
+            return True
+    return False
+
+
+def check_if_eid_present(eid):
+    for emp in faculty.faculty_list:
+        if emp.eid == eid:
+            return True
+    return False
 
 
 def verify_authentication(eid):
@@ -11,13 +26,6 @@ def verify_authentication(eid):
 def check_if_admnid_present(admn_id_no):
     for std in student.student_list:
         if std.admn_id_no == admn_id_no:
-            return True
-    return False
-
-
-def check_if_eid_present(eid):
-    for emp in faculty.faculty_list:
-        if emp.eid == eid:
             return True
     return False
 
@@ -59,13 +67,35 @@ def add_student():
     # add created student to list
     student.student_list.append(s)
 
-def issue_book(book_title):
 
-    verify_auth = input('Enter Unique Employee ID: ')
-    if verify_authentication(verify_auth):
-        
-        # if authenticated search for book
-        # if book found , search for student
-        # if student found and student num_books < 4 and student not issued this book already issue book
+def add_book():
+
+    # ask for parameters of a book from user
+    title = input('Enter Book Title: ')
+    author = input('Enter Name of Author: ')
+    while True:
+        isbn = input('Enter Book ISBN: ')
+        if check_if_isbn_present(isbn):
+            continue
+        else:
+            break
+    num_copies = input('Enter Number of Copies of Book To Be Added To Library: ')
+
+    # calling book constructor
+    bk_new = book.BookClass(title, author, isbn, num_copies)
+
+    # add new book to list of books
+    book.book_list.append(bk_new)
+
+
+# def issue_book(book_title):
+#
+#     verify_auth = input('Enter Unique Employee ID: ')
+#     if verify_authentication(verify_auth):
+#           # if authenticated search for book
+#           # if book found , search for student
+#           # if student found and student num_books < 4 and student not issued this book already issue book
+
+
 
 
